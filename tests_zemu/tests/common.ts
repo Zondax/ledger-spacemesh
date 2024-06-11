9a1d501a69a908ae6876de331622a5d1ad672d3f
abenso <andresbenso@gmail.com>
2024-06-11 20:16:11 -0300

    wip

--- tests_zemu/tests/common.ts
+++ tests_zemu/tests/common.ts
@@ -12,8 +12,8 @@ const APP_PATH_ST = resolve('../app/output/app_stax.elf')
 
 export const models: IDeviceModel[] = [
   //{ name: 'nanos', prefix: 'S', path: APP_PATH_S },
-  { name: 'nanox', prefix: 'X', path: APP_PATH_X },
-  // { name: 'nanosp', prefix: 'SP', path: APP_PATH_SP },
+  //{ name: 'nanox', prefix: 'X', path: APP_PATH_X },
+  { name: 'nanosp', prefix: 'SP', path: APP_PATH_SP },
   // { name: 'stax', prefix: 'ST', path: APP_PATH_ST },
 ]
 
